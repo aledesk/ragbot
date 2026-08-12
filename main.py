f5a713351c14f2a6c3f2d1c4193795daee86a5ba
aledesk <alevaraestudio@gmail.com>
2026-08-12 18:32:48 -0300

    corrijo importacion de arranque para produccion

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import uvicorn
 import os
+from rag import app
 
 if __name__ == "__main__":
-    # Obtenemos el puerto que nos asigne el servidor gratuito (por defecto 8000)
     puerto = int(os.environ.get("PORT", 8000))
-    # Arrancamos uvicorn apuntando directamente al archivo rag.py
-    uvicorn.run("rag:app", host="0.0.0.0", port=puerto, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=puerto, reload=False)
